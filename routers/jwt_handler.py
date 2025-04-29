66b69b63679d762a814ff7d50bf3c6c503724804
Alagani Jagadeesh <a.jagadeesh@exafluence.com>
2025-04-29 19:03:11 +0530

    changed datetime.utcnow() to datetime.now()

--- routers/jwt_handler.py
+++ routers/jwt_handler.py
@@ -15,7 +15,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 def create_access_token(data: dict, expires_delta: int = 15):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    expire = datetime.now() + timedelta(minutes=expires_delta)
     to_encode.update({'exp':expire})
     return jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
